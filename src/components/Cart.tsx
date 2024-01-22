45af57dceb10836448fd6ff4e02c15ee1f0217a1
Mamun-Ashraf <ashrafdkia@gmail.com>
2024-01-22 17:09:29 +0600

    implemented total price and checkout

--- src/components/Cart.tsx
+++ src/components/Cart.tsx
@@ -18,13 +18,8 @@ import { addToCart, removeFromCart, removeOne } from '@/redux/features/cart/cart
 
 export default function Cart() {
 
-  const { products } = useAppSelector((state) => state.cart);
+  const { products, total } = useAppSelector((state) => state.cart);
   const dispatch = useAppDispatch();
-  //! Dummy data
-
-  const total = 0;
-
-  //! **
 
   return (
     <Sheet>
